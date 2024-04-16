81e4aa5efe3cc9ae82925aafc5e6c9290d745959
saadarazzaq <sabdurrazzaq124@gmail.com>
2024-04-16 23:08:58 +0500

    Your commit message here

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./app.scss";
+import Contact from "./components/contact/Contact";
+
+const App = () => {
+  return (
+    <div>
+      <section id="Contact">
+        <Contact />
+      </section>
+    </div>
+  );
+};
+
+export default App;
